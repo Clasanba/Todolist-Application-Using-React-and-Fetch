573316e7d0299394b15457e697ed694ce7fc9ce8
Clasanba <clasanba@gmail.com>
2022-11-29 12:00:53 +0000

    btn delete all

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
-import { createRoutesFromChildren } from "react-router";
+
+
 
 //create your first component
 const Home = () => {
@@ -26,8 +27,8 @@ const Home = () => {
 
 
     }, [])
-    const saveTasks=(tasks)=>{
-        if(tasks.length===0){
+    const saveTasks = (tasks) => {
+        if (tasks.length === 0) {
             return
         }
         return fetch('https://assets.breatheco.de/apis/fake/todos/user/claudia', {
@@ -44,7 +45,7 @@ const Home = () => {
                 return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
             })
             .then(data => {
-               
+
                 console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
             })
             .catch(error => {
@@ -53,49 +54,48 @@ const Home = () => {
             });
     }
 
-
-
     const onAddButtonClick = (e) => {
         e.preventDefault(); // este comando evita que 'e, que en este caso hace referencia a onSubmit, actue por defecto. Para este caso en concreto sería recargar la página cada vez que le demos a intro. 
 
         const taskTitle = inputRef.current.value; // guarda el valor del input.(Si solo pusieramos current imprimiría la línea de código del input)
         if (taskTitle === "") { return }
-        const newTasks=[...tasks, {label:taskTitle,done:false}]
-        saveTasks(newTasks).then(()=>{
+        const newTasks = [...tasks, { label: taskTitle, done: false }]
+        saveTasks(newTasks).then(() => {
             setTasks(newTasks)
         }); // Estamos diciendole que setTasks cree un nuevo array donde meta todo lo que hay dentro de tasks (...tasks) más el nuevo contenido del input(taskTitle).
         inputRef.current.value = ""; // estamos diciendole que cuando ejecute la linea de arriba deje en blanco el input para volver ser usado
     }
-    const onDeleteButtonClick = (position) => { //Función para que el botón "X" borre la task en la que se encuentra
-        tasks.splice(position, 1); // Estamos diciendole que borre la tasks que se encuentre en position y que solo borre un dato. La position la obtenemos mas abajo en la función map
-        saveTasks(tasks).then(()=>{
+    const onDeleteButtonClick = (position) => {
+        tasks.splice(position, 1);
+        saveTasks(tasks).then(() => {
             setTasks([...tasks])
-        }); // Estamos diciendo que cuando ejecute splice despues nos devuelva un nuevo array con el contenido que quede en tasks
+        });
     }
 
-
-    // Utilizamos onSubmit para que guarde las tareas dandole a intro. También podría actuar con un botón metiendolo como si fuera su hijo
-    return (
+    return (<>
         <div className="container">
             <h1>Todos</h1>
             <form onSubmit={onAddButtonClick} className="input-group mb-3">
                 <input ref={inputRef} type="text" className="form-control" aria-label="Text input with segmented dropdown button" />
-    </form>
+            </form>
 
             {tasks.length === 0
-                ? (<span>No hay tareas, añadir tareas</span>) // la función map puedo pasarle hasta tres argumentos: el elemento actual, el index o i y un array. En este caso hemos pasado el elemento actual y la i.
-                : <ul>{
-                    tasks.map((taskElement, i) => {
-                        return (<li key={i}>{taskElement.label} <button type="button" className="btn-close" aria-label="Delete" onClick={() => onDeleteButtonClick(i)}></button></li>)
-                    })
-                }
-                </ul>
-                // llamamos a la funcion onDeleteButtonClick parandole como atributo la posición que me da map en este caso es i.
-
+                ? (<span>No hay tareas, añadir tareas</span>)
+                : <div>
+                    <ul>{
+                        tasks.map((taskElement, i) => {
+                            return (<li key={i}>{taskElement.label} <button type="button" className="btn-close" aria-label="Delete" onClick={() => onDeleteButtonClick(i)}></button></li>
+                            )
+                        })
+                    }
+                    </ul>
+                    <button type="button" className="" aria-label="Delete" onClick={() => onDeleteButtonAll()}>Borrar todo</button>
+                </div>
             }
 
 
         </div>
+    </>
     );
 };
 
