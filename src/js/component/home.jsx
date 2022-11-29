42b395722f7683391d8ee869f0948f752114325d
Clasanba <clasanba@gmail.com>
2022-11-29 17:48:11 +0000

    complete function

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,26 +7,54 @@ const Home = () => {
     const [tasks, setTasks] = useState([])  //variable de estado de tipo array
     const inputRef = useRef(); // variable de estado que sirve para hacer referencia al algún punto del código
 
-    //Segunda parte del ejercicio
+
+    const createList = () => {
+         return fetch('https://assets.breatheco.de/apis/fake/todos/user/claudia', {
+            method: "POST",
+            body: JSON.stringify([]),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+            .then(resp => {
+                console.log(resp.ok); 
+                console.log(resp.status); 
+                return resp.json(); 
+            })
+            .then(data => {
+
+                console.log(data); 
+            })
+            .catch(error => {
+                //manejo de errores
+                console.log(error);
+            });
+    }
 
     useEffect(() => {
         fetch('https://assets.breatheco.de/apis/fake/todos/user/claudia', { method: "GET", headers: { Accept: "application/json" } })
             .then(resp => {
-
-                return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+                if(resp.status === 404){
+                    return []
+                }
+                return resp.json(); 
             })
             .then(data => {
-                const list = data;
-                setTasks(list)//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+                setTasks(data)
                 //console.log(typeof list); //esto imprimirá en la consola el objeto exacto recibido del servidor
             })
             .catch(error => {
                 //manejo de errores
                 console.log(error);
             });
-
+        
 
     }, [])
+
+    
+
+
     const saveTasks = (tasks) => {
         if (tasks.length === 0) {
             return
@@ -40,13 +68,13 @@ const Home = () => {
         })
 
             .then(resp => {
-                console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
-                console.log(resp.status); // el código de estado = 200 o código = 400 etc.
-                return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+                console.log(resp.ok); 
+                console.log(resp.status); 
+                return resp.json(); 
             })
             .then(data => {
 
-                console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+                console.log(data); 
             })
             .catch(error => {
                 //manejo de errores
@@ -54,23 +82,68 @@ const Home = () => {
             });
     }
 
-    const onAddButtonClick = (e) => {
-        e.preventDefault(); // este comando evita que 'e, que en este caso hace referencia a onSubmit, actue por defecto. Para este caso en concreto sería recargar la página cada vez que le demos a intro. 
+    const deleteAllTasks = (tasks) => {
+        
+        return fetch('https://assets.breatheco.de/apis/fake/todos/user/claudia', {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
 
-        const taskTitle = inputRef.current.value; // guarda el valor del input.(Si solo pusieramos current imprimiría la línea de código del input)
+            .then(resp => {
+                console.log(resp.ok); 
+                console.log(resp.status); 
+                return resp.json(); 
+            })
+            .then(data => {
+
+                console.log(data); 
+            })
+            .catch(error => {
+                
+                console.log(error);
+            });
+    }
+
+    const onDeleteButtonAll = ()=>{
+        const deleteAll = setTasks([])
+        deleteAllTasks(deleteAll).then(()=>{
+            
+        })
+     }
+
+    const onAddButtonClick = (e) => {
+        e.preventDefault(); 
+        
+        const taskTitle = inputRef.current.value; 
         if (taskTitle === "") { return }
         const newTasks = [...tasks, { label: taskTitle, done: false }]
-        saveTasks(newTasks).then(() => {
-            setTasks(newTasks)
-        }); // Estamos diciendole que setTasks cree un nuevo array donde meta todo lo que hay dentro de tasks (...tasks) más el nuevo contenido del input(taskTitle).
-        inputRef.current.value = ""; // estamos diciendole que cuando ejecute la linea de arriba deje en blanco el input para volver ser usado
+        if (tasks.length === 0){
+            createList(taskTitle)
+            .then(()=>{
+                return saveTasks(newTasks)
+            })
+            .then(() => {
+                setTasks(newTasks)
+            }); 
+        } else {
+            saveTasks(newTasks).then(() => {
+                setTasks(newTasks)
+            }); 
+        }
+        inputRef.current.value = ""; 
     }
     const onDeleteButtonClick = (position) => {
+        if (tasks.length ===1){
+            deleteAllTasks().then(()=> setTasks([]))
+        }else{
         tasks.splice(position, 1);
         saveTasks(tasks).then(() => {
             setTasks([...tasks])
-        });
+        })};
     }
+    
 
     return (<>
         <div className="container">
